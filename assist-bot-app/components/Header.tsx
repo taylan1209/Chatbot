85e358bb0ed2fe1fe5f2488c44d1245a67a0afcc
taylan1209 <taylancokyaman@gmail.com>
2024-08-12 13:52:05 +0300

    12.08-13.52

--- assist-bot-app/components/Header.tsx
+++ assist-bot-app/components/Header.tsx
@@ -1,18 +1,29 @@
 import Link from 'next/link'
 import React from 'react'
 import Avatar from './Avatar'
+import { SignedIn, SignedOut,SignInButton, UserButton } from '@clerk/nextjs'
 
 function Header() {
   return (
-    <header>
-        <Link href ="/">
+    <header className='b-white shadow-sm text-gray-800 flex'>
+        <Link href ="/" className='flex items-center text-4xl font-thin'>
         <Avatar seed='Chatbot'/>
 
-        <div>
+        <div className='space-y-1'>
         <h1>Tanıtma ChatBot</h1>
-        <h2>Admin Paneli</h2>
+        <h2 className='text-sm'>Admin Paneli</h2>
             </div>
         </Link>
+
+        <div className='flex items-center'>
+          <SignedIn>
+            <UserButton  showName/>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </div>
     </header>
   )
 }
