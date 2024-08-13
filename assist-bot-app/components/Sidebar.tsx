be3062ba46afcb60994bda52b3517ee7b2ef4b71
taylan1209 <taylancokyaman@gmail.com>
2024-08-13 15:07:55 +0300

    13.08-15.07

--- assist-bot-app/components/Sidebar.tsx
+++ assist-bot-app/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { BotMessageSquare, PencilLine, SearchIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,13 +6,30 @@ function Sidebar() {
   return <div className='bg-white text-white p-5'>
     <ul className='gap-5 flex lg:flex-col'>
         <li className='flex-1'>
-            <Link href="/create-chatbot" className='hover:opacity-50 flex flex-col text-center lg: text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]'>Link</Link>
+            <Link href="/create-chatbot" className='hover:opacity-50 flex flex-col text-center lg: text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]'>
+              
+              <BotMessageSquare className='h-6 w-6 lg:h-8 lg:w-8'/>
+              <div className='hidden md:inline'>
+                <p className='text-xl'>Create </p>
+                <p className='text-sm font-extralight'>New Chatbot</p>
+                </div>
+              </Link>
         </li>
         <li className='flex-1'>
-            <Link href="/view-chatbots" className='hover:opacity-50 flex flex-col text-center lg: text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]'>Link</Link>
+            <Link href="/view-chatbots" className='hover:opacity-50 flex flex-col text-center lg: text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]'>
+              <PencilLine className='h-6 w-6 lg:h-8 lg:w-8'/>
+              <div className='hidden md:inline'>
+                <p className='text-xl'>Edit </p>
+                <p className='text-sm font-extralight'>Chatbots</p>
+                </div></Link>
         </li>
         <li className='flex-1'>
-            <Link href="/review-sessions" className='hover:opacity-50 flex flex-col text-center lg: text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]'>Link</Link>
+            <Link href="/review-sessions" className='hover:opacity-50 flex flex-col text-center lg: text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]'>
+            <SearchIcon className='h-6 w-6 lg:h-8 lg:w-8'/>
+              <div className='hidden md:inline'>
+                <p className='text-xl'>View </p>
+                <p className='text-sm font-extralight'>Sessions</p>
+                </div></Link>
         </li>
     </ul>
   </div>
