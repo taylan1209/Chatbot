f591a900bbacb12d2f3bca1f8333b75b6df2457c
taylan1209 <taylancokyaman@gmail.com>
2024-08-13 15:53:41 +0300

    Update seed.sql

--- assist-bot-app/seed.sql
+++ assist-bot-app/seed.sql
@@ -36,5 +36,37 @@ CREATE TABLE messages (
   chat_session_id INT REFERENCES chat_sessions(id) ON DELETE CASCADE,
   sender VARCHAR(255) NOT NULL,
   content TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  sender VARCHAR(50) NOT NULL-- 'User' or 'ai'
 );
+-- --------------------------------
+-- This step is more of a BUG FIX to ensure that the created_at column is set to thecurrent timestamp when a new record is inserted.
+--We experienced a strange issue with this but usually its not necessary to do this.
+-- Create the trigger function to set created_at.
+
+CREATE OR REPLACE FUNCTION set_created_at()
+RETURNS TRIGGER AS $$
+BEGIN
+   IF NEW.created_at IS NULL THEN
+      NEW.created_at = CURRENT_TIMESTAMP;
+   END IF;
+   RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Create triggers for each table to set created_at
+CREATE TRIGGER set_chatbots_created_at
+BEFORE INSERT ON chatbots
+FOR EACH ROW
+EXECUTE FUNCTION set_created_at();
+
+CREATE TRIGGER set_chatbots_created_at
+BEFORE INSERT ON chatbots
+FOR EACH ROW
+EXECUTE FUNCTION set_created_at();
+
+CREATE TRIGGER set_chatbot_characteristics_created_at
+BEFORE INSERT ON chatbot_characteristics
+FOR EACH ROW
+EXECUTE FUNCTION set_created_at(); 
