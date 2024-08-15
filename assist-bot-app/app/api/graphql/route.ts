4a333c095db7f32a72cff258c7ad62fba0982542
taylan1209 <taylancokyaman@gmail.com>
2024-08-15 12:22:31 +0300

    Update route.ts

--- assist-bot-app/app/api/graphql/route.ts
+++ assist-bot-app/app/api/graphql/route.ts
@@ -1,21 +1,46 @@
 import client from "@/graphql/apolloClient";
-import { NextRequest } from "next/server";
+import { serverClient } from "@/lib/server/serverClient";
+import { gql } from "@apollo/client";
+import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic"; 
+
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 
 export async function POST(request: NextRequest){
     const  {query, variables} = await request.json();
 
     try {
+        let result;
         if (query.trim().startsWith("mutation")){
             //Handle mutations
-            client 
-            
+            result = await serverClient.mutate({
+                mutation: gql`${query}`,
+                variables,
+            })
+
         }else {
             //Handle queries
+            result = await serverClient.query({
+                query: gql`${query}`,
+                variables,
+            })
         }
-    } catch (error) {
+        const data = result.data;
+        return NextResponse.json({
+            data,
+        }, {
+            headers:corsHeaders,
+        })
         
+    } catch (error) {
+         console.log(error);
+         return NextResponse.json(error,{
+            status: 500,
+        })
     }
 
 }
