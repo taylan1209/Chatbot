51f5fa79d7fea17443779c9098499900bea4f183
taylan1209 <taylancokyaman@gmail.com>
2024-08-15 11:14:29 +0300

    15.08-11.14

--- assist-bot-app/app/layout.tsx
+++ assist-bot-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import ApolloProviderWrapper from "@/components/ApolloProvider";
 
 
 
@@ -15,14 +16,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ApolloProviderWrapper>
+
     <ClerkProvider>
     <html lang="en">
       <body className="min-h-screen flex">{children}
-      
-      
       {/* {Toaster} */}
       </body>
     </html>
     </ClerkProvider>
+    </ApolloProviderWrapper>
   );
 }
