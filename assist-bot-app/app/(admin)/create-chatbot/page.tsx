b26a22b9f6d64b467abfb427c2929331e58fcdba
taylan1209 <taylancokyaman@gmail.com>
2024-08-16 11:19:11 +0300

    Mutations

--- assist-bot-app/app/(admin)/create-chatbot/page.tsx
+++ assist-bot-app/app/(admin)/create-chatbot/page.tsx
@@ -1,17 +1,55 @@
+'use client';
+
 import Avatar from '@/components/Avatar'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import React from 'react'
+import { CREATE_CHATBOT } from '@/graphql/mutations/mutations'
+import { useMutation } from '@apollo/client'
+import { useUser } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation';
+import React, { FormEvent, useState } from 'react'
 
 function CreateChatbot() {
+  const {user} = useUser(); 
+  const [name, setName] = useState('');
+  const router = useRouter();
+
+  const [createChatbot, {data, loading, error}] = useMutation(CREATE_CHATBOT, {
+    variables: {
+        clerk_user_id: user?.id,
+        name,
+    }
+  });
+
+    const handleSubmit = async (e:FormEvent) =>{
+      e.preventDefault();
+
+      try {
+        const data = await createChatbot();
+        setName("");
+
+        router.push(`/edit-chatbot/${data.data.insertChatbots.id}`)
+
+
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    if(!user){
+        return null;
+    }
+
   return (
     <div className='flex flex-col items-center justify-center md:flex-row md:space-x-10 bg-white p-10 rounded-md m-10'>
         <Avatar seed="create-chatbot "/>
         <div>
             <h1 className='text-xl lg:text-3xl font-semibold'>Create</h1>
             <h2 className='font-light'>Create a new chatbot to assist you for promoting</h2>
-            <form className='flex flex-col md:flex-row gap-2 mt-5'>
+            <form onSubmit={handleSubmit} className='flex flex-col md:flex-row gap-2 mt-5'>
                 <Input type='text' 
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 placeholder='Chatbot Name..'
                 className='max-w-lg' 
                 required/>
