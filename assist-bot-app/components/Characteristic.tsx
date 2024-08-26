29f6ed21d42de5528ad9dc3a077f9b8d9b147f30
taylan1209 <taylancokyaman@gmail.com>
2024-08-26 16:51:44 +0300

    deleting characteristics

--- assist-bot-app/components/Characteristic.tsx
+++ assist-bot-app/components/Characteristic.tsx
@@ -7,44 +7,44 @@ import { OctagonX } from 'lucide-react'
 import React from 'react'
 import { toast } from 'sonner';
 
-function Characteristic({characteristic,}:
-    {characteristic:ChatbotCharacteristic;
-    
-}) {
+function Characteristic({ characteristic }: { characteristic: ChatbotCharacteristic }) {
 
     const [removeCharacteristic] = useMutation(REMOVE_CHARACTERISTIC, {
         refetchQueries: ["GetChatbotById"],
     });
 
-    const handleRemoveCharacteristic = async(characteristicId:number) => {
+    const handleRemoveCharacteristic = async (characteristicId: number) => {
         try {
-            await removeCharacteristic({
+            const promise = removeCharacteristic({
                 variables: {
-                    id: characteristicId,
+                    characteristicId,
                 },
             });
-            
+            await promise;
+            return promise;
         } catch (error) {
-            console.log(error);
+            console.error("Error removing characteristic", error);
+            toast.error("Error removing characteristic");
+            throw error;
         }
     }
 
-  return (
-    <li key={characteristic.id} className='relative'>
-      {characteristic.content}
-      <OctagonX className='w-6 h-6 text-white fill-red-500 
-      absolute top-1 right-1 cursor-pointer hover:opacity-50 ' 
-      onClick={()=>{
-        const promise = handleRemoveCharacteristic(characteristic.id);
-        toast.promise(promise, {
-            loading: "Removing characteristic...",
-            success: "Characteristic removed successfully",
-            error: "Error removing characteristic",
-        });
-      }}
-      />
-    </li>
-  )
+    return (
+        <li key={characteristic.id} className='relative'>
+            {characteristic.content}
+            <OctagonX
+                className='w-6 h-6 text-white fill-red-500 absolute top-1 right-1 cursor-pointer hover:opacity-50'
+                onClick={() => {
+                    const promise = handleRemoveCharacteristic(characteristic.id);
+                    toast.promise(promise, {
+                        loading: "Removing characteristic...",
+                        success: "Characteristic removed successfully",
+                        error: "Error removing characteristic",
+                    });
+                }}
+            />
+        </li>
+    )
 }
 
 export default Characteristic
