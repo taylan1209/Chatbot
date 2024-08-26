d542b753f1691b729bc26efc913c071567756a9a
taylan1209 <taylancokyaman@gmail.com>
2024-08-26 16:59:27 +0300

    Update Characteristic.tsx

--- assist-bot-app/components/Characteristic.tsx
+++ assist-bot-app/components/Characteristic.tsx
@@ -30,7 +30,7 @@ function Characteristic({ characteristic }: { characteristic: ChatbotCharacteris
     }
 
     return (
-        <li key={characteristic.id} className='relative'>
+        <li key={characteristic.id} className='relative p-10 bg-white border rounded-md'>
             {characteristic.content}
             <OctagonX
                 className='w-6 h-6 text-white fill-red-500 absolute top-1 right-1 cursor-pointer hover:opacity-50'
