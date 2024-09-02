d018eb2e5853c725f923ea8b459f737f1a3af006
taylan1209 <taylancokyaman@gmail.com>
2024-09-02 14:25:23 +0300

    Message page

--- assist-bot-app/components/Messages.tsx
+++ assist-bot-app/components/Messages.tsx
@@ -5,6 +5,8 @@ import { usePathname } from 'next/navigation';
 import React from 'react'
 import Avatar from './Avatar';
 import { UserCircle } from 'lucide-react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 function Messages({messages, chatbotName}:{
     messages: Message[];
@@ -15,7 +17,7 @@ function Messages({messages, chatbotName}:{
     const isReviewsPage = path.includes("review-sessions");
 
   return (
-    <div>
+    <div className='flex-1 flex flex-col overflow-y-auto space-y-10 py-10 px-5 bg-white rounded-lg'>
         {messages.map((message) => {
         const isSender = message.sender !=="user";
 
@@ -30,7 +32,8 @@ function Messages({messages, chatbotName}:{
             )}
 
 
-         <div>
+         <div className={`chat-image avatar w-10 ${
+            !isSender && "-mr-4"}`}>
         {isSender ? (
             <Avatar seed={chatbotName} className='h-12 w-12
             bg-white rounded-full border-2
@@ -39,6 +42,58 @@ function Messages({messages, chatbotName}:{
              <UserCircle className="text-[#2991EE]" />
         )}
      </div>
+     <p className={`chat-bubble text-white ${
+        isSender ? "chat-bubble-primary bg-[#4D7DFB]"
+        : "chat-bubble-secondary bg-gray text-gray-700"
+     }`}>
+        <ReactMarkdown 
+        remarkPlugins={[remarkGfm]}
+        className={`break-words`}
+        components={{
+            ul: ({ node, ...props }) => (
+                <ul className="list-disc list-inside ml-5 mb-5" {...props} />
+            ),
+            ol: ({ node, ...props }) => (
+                <ol className="list-decimal list-inside ml-5 mb-5" {...props} />
+            ),
+            h1: ({ node, ...props }) => (
+                <h1 className="text-2xl font-bold mb-5" {...props} />
+            ),
+            h2: ({ node, ...props }) => (
+                <h2 className="text-xl font-bold mb-5" {...props} />
+            ),
+            h3: ({ node, ...props }) => (
+                <h3 className="text-lg font-bold mb-5" {...props} />
+            ),
+            table: ({ node, ...props }) => (
+                <table className="table-auto w-full border-separate border-2 
+                rounded-sm border-spacing-4 border-white mb-5" {...props} />
+            ),
+            th: ({ node, ...props }) => (
+                <th className="font-bold" {...props} />
+            ),
+            td: ({ node, ...props }) => (
+                <td className="text-gray-700" {...props} />
+            ),
+            // Add other headings as needed
+            li: ({ node, ...props }) => (
+                <li className="list-item" {...props} />
+            ),
+            p: ({ node, ...props }) => (
+                <p className={`whitespace-break-spaces mb-5 ${
+                    message.content === "Thinking..." && "animate-pulse"
+                } ${isSender ? "text-white" : "text-gray-700"}`}{...props} />
+            ),
+            a: ({ node, ...props }) => (
+                <a className="font-bold underline hover:text-blue-400" 
+                target="_blank" 
+                rel='nooper noreferrer' {...props} />
+            ),
+            // Add other components as needed
+        }}>
+        {message.content}
+        </ReactMarkdown>
+     </p>
     </div>
   );
 })}
