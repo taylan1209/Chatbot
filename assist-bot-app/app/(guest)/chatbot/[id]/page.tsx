e3eb1d6451897a36f92ab7da4a8821b9ee758adc
taylan1209 <taylancokyaman@gmail.com>
2024-09-03 11:16:16 +0300

    Chatbot Page

--- assist-bot-app/app/(guest)/chatbot/[id]/page.tsx
+++ assist-bot-app/app/(guest)/chatbot/[id]/page.tsx
@@ -1,8 +1,30 @@
 'use client'
 
-function ChatbotPage() {
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Message } from "@/types/types";
+import { useState } from "react"
+
+
+function ChatbotPage({params: {id}}:{params:{id:string}}) {
+  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isOpen, setIsOpen] = useState("");
+  const [chatId, setChatId] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+
+  
   return (
-    <div>ChatbotPage</div>
+    <div>ChatbotPage: {id} </div>
   )
 }
 
